f8dadd1e37d2dabf86b1b9c18148767d0f9e8c06
user <user@user-PC.mshome.net>
2019-07-26 11:56:00 +0530

    search result

---
+++ Object Repository/search_element/search_result/div_Save.rs
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Save</name>
+   <tag></tag>
+   <elementGuidId>ca2edcbf-3a5b-4cf6-be4d-5b416635ebf8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content-grids']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-md-6 col-sm-12 promoted lp-grid-box-contianer grid_view2 card1 lp-grid-box-contianer1</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-title</name>
+      <type>Main</type>
+      <value>Sauce &amp; Barrel</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-postid</name>
+      <type>Main</type>
+      <value>847</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-lattitue</name>
+      <type>Main</type>
+      <value>40.7081175</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-longitute</name>
+      <type>Main</type>
+      <value>-74.01436209999997</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-posturl</name>
+      <type>Main</type>
+      <value>https://classic.listingprowp.com/listing/sauce-barrel/</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-lppinurl</name>
+      <type>Main</type>
+      <value>http://studio.listingprowp.com/wp-content/themes/listingpro/assets/images/pins/pin.png</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+												
+							
+								
+									
+										
+																
+																								
+									
+										
+																
+																								
+							   	
+								
+									
+										
+											
+												 Save
+											
+										
+										
+											 Preview
+										
+									
+								
+							
+							
+								
+									
+										
+											
+												Ad												Sauce &amp; Barrel																							
+										
+										
+																						
+												4.0/ 5														
+															4															Ratings														
+																							
+																						
+												
+					$$
+																
+											
+												
+																
+																	Restaurant
+																											
+										
+															
+						
+							
+								
+							
+														
+								
+								dd...
+							
+						
+					
+								
+													
+									
+									
+								
+																	
+										
+											
+												
+																
+																	New York
+																											
+																							
+													
+																											
+													97 Washington St, New York, NY...
+												
+																					
+										
+												
+													Closed Now! 
+													
+																						
+									
+								
+															
+						
+					</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content-grids&quot;)/div[@class=&quot;promoted-listings&quot;]/div[@class=&quot;col-md-6 col-sm-12 promoted lp-grid-box-contianer grid_view2 card1 lp-grid-box-contianer1&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='content-grids']/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All Categories'])[2]/following::div[11]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Most Viewed'])[1]/following::div[15]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div[2]/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
